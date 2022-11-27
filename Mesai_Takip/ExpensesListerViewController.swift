3cbde3ca4c9c4835214cebd2856359c666869856
emirkeskinn <emkeskin41@gmail.com>
2022-11-27 13:30:05 +0300

    first commit

---
+++ Mesai_Takip/ExpensesListerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExpensesListerViewController.swift
+//  Mesai_Takip
+//
+//  Created by Emir Keskin on 27.11.2022.
+//
+
+import UIKit
+
+class ExpensesListerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
